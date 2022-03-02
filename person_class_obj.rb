955343683fa191ff2342ab1a49b776c4c3979255
kingsleyibe <ibekingsley66@gmail.com>
2022-03-02 13:55:34 +0100

    Add output.rb file print result

--- person_class_obj.rb
+++ person_class_obj.rb
@@ -1,30 +1,32 @@
-require_relative './corrector'
+require './corrector'
 
 class Person
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :parent_permission
 
-  def initialize(age, parent_permission: true, name: 'Unknown')
-    @id = Random.rand(1..100)
-    @name = name
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1...1000)
     @age = age
+    @name = name
     @parent_permission = parent_permission
-    @corrector = Corrector.new(name)
+    @corrector = Corrector.new
   end
 
   def validate_name(name)
     @name = @corrector.correct_name(name)
   end
 
+  def can_use_services?
+    if of_age? || @parent_permission
+      true
+    else
+      false
+    end
+  end
+
+  private
+
   def of_age?
     @age >= 18
   end
-
-  private :of_age?
-
-  def can_use_services?
-    of_age? && @parent_permission
-  end
-
-  public :can_use_services?
 end
--- student_class_obj.rb
+++ student_class_obj.rb
@@ -2,7 +2,7 @@ require_relative './person_class_obj'
 
 class Student < Person
   def initialize(classroom)
-    super
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
