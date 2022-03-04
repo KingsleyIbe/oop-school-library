6b7c4bb090ee1af4a120d8db89cda2e7b308da9a
kingsleyibe <ibekingsley66@gmail.com>
2022-03-04 11:56:00 +0100

    Refactor code

--- main.rb
+++ main.rb
@@ -6,25 +6,28 @@ require './student_class_obj'
 require './teacher_class_obj'
 require './rental'
 
-# Opener
+# Welcome note
 puts "---------------------------------\n
- Welcome to School Library App!\n
+ Welcome to OOP School Library App!\n
  ------------------------------"
 
 # State Helpers
 @book_arr = []
 @person_arr = []
 
-def book_store(book)
+# Method to add new book record
+def add_book(book)
   @book_arr.push(book)
   @book_arr
 end
 
-def person_store(person)
+# Method to add new book record
+def add_person(person)
   @person_arr.push(person)
   @person_arr
 end
 
+# Method to create new student's record
 def create_student
   print 'Age: '
   age = gets.chomp
@@ -34,20 +37,21 @@ def create_student
   perm_input = gets.chomp
   case perm_input.upcase
   when 'Y'
-    perm = true
+    permission = true
   when 'N'
-    perm = false
+    permission = false
   else
-    puts 'Wrong input!'
+    puts 'Invalid entry!'
     init
   end
   student = Student.new(age, name)
-  student.parent_permission = perm
+  student.parent_permission = permission
   student.type = 'Student'
-  person_store(student)
+  add_person(student)
   success('Person')
 end
 
+# Method to create new teacher record
 def create_teacher
   print 'Age: '
   age = gets.chomp
@@ -58,11 +62,11 @@ def create_teacher
   specialization = gets.chomp
   teacher.specialization = specialization
   teacher.type = 'Teacher'
-  person_store(teacher)
+  add_person(teacher)
   success('Person')
 end
 
-# Formatters
+# Confirmation notes
 def success(item)
   puts "#{item} created succesfully!"
 end
@@ -73,13 +77,15 @@ def list_all_books
   init
 end
 
+# Method to list all persons' record
 def list_all_people
   @person_arr.map { |person| puts "[#{person.type}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
   init
 end
 
+# Method to create new person
 def create_a_person()
-  puts 'Do you want to create a student (1) or a teacher (2)? [enter the number]: '
+  puts 'Do you want to create a student (1) or a teacher (2)? [enter a number]: '
   option = gets.chomp
   case option.to_i
   when 1
@@ -87,22 +93,24 @@ def create_a_person()
   when 2
     create_teacher
   else
-    puts 'Wrong input !'
+    puts 'Invalid entry !'
   end
   init
 end
 
+# Method to create a new book
 def create_a_book
   print 'Title: '
   title = gets.chomp
   print 'Author: '
   author = gets.chomp
   book = Book.new(title, author)
-  book_store(book)
+  add_book(book)
   success('Book')
   init
 end
 
+# Method to create a new rental
 def create_a_rental
   puts 'Select a book from the following list by number'
   @book_arr.each_with_index { |book, idx| puts "#{idx}) Title: \"#{book.title}\" Author: #{book.author}" }
@@ -121,6 +129,7 @@ def create_a_rental
   init
 end
 
+# Method to list all rentals record
 def list_rentals_for_id
   print 'ID of person: '
   person_id = gets.chomp.to_i
@@ -134,17 +143,17 @@ def list_rentals_for_id
 end
 
 def exit
-  puts 'Thank you for using School Library app!'
+  puts 'Thank you for using OOP School Library app!'
 end
 
-# Menu
+# Menu for user entries
 def menu
   puts "\nPlease choose an option by entering a number:"
   puts '1 - List all books', '2 - List all people', '3 - Create a person', '4 - Create a book',
        '5 - Create a rental', '6 - List all rentals for a given person id', '7 - Exit'
 end
 
-# Menu Selections
+# Menu Selections for user entries
 @menu_hash = { 1 => method(:list_all_books), 2 => method(:list_all_people), 3 => method(:create_a_person),
                4 => method(:create_a_book), 5 => method(:create_a_rental),
                6 => method(:list_rentals_for_id), 7 => method(:exit) }
@@ -154,7 +163,7 @@ def init()
   if selection.positive? && selection <= 7
     @menu_hash[selection].call
   else
-    puts 'Wrong input !'
+    puts 'Invalid entry!'
   end
 end
 
